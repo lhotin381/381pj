8087b23e4a37c85865402a9e00ffd9a454282c2b
jianguangyao <you@example.com>
2015-11-30 22:16:27 +0800

    update attrib with param

--- server.js
+++ server.js
@@ -160,4 +160,28 @@ app.put('/:id/:attrib/:attrib_value', function(req,res) {
     });
 });
 
+//update attrib with param
+app.put('/:param/:param_value/:attrib/:attrib_value', function(req,res) {
+	var restaurantSchema = require('./models/restaurant');
+	mongoose.connect('mongodb://localhost/test');
+	var db = mongoose.connection;
+	db.on('error', console.error.bind(console, 'connection error:'));
+	db.once('open', function (callback) {
+		var Restaurant = mongoose.model('Restaurant', restaurantSchema);
+		var criteria = {};
+		criteria[req.params.attrib] = req.params.attrib_value;
+		var param = {};
+		param[req.params.param] = req.params.param_value;
+		Restaurant.update(param,{$set:criteria},function(err,results){
+       		if (err) {
+				res.status(500).json(err);
+				throw err
+			}
+       		//console.log('Restaurant updated!')
+       		db.close();
+			res.status(200).json({message: 'update done', id: req.params.id});
+    	});
+    });
+});
+
 app.listen(process.env.PORT || 8099);
